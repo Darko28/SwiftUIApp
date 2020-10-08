8dc35bbe0db56982aaba041e691c0ad8e563e4c5
DarkoBomer <qq1572055493@icloud.com>
2020-10-08 11:42:31 +0800

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Darko on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
